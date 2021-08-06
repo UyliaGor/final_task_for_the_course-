f75162f31f1d35b8d46cc208a05258def7757a6e
Julia Gorelikova <jkireenko137@gmail.com>
2021-08-06 17:38:55 +0300

    Добавлены переходы между страницами

--- fails for githab/test_main_page.py
+++ fails for githab/test_main_page.py
@@ -7,14 +7,19 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url) #переход между страницами
+    login_page.should_be_login_page()
 
-def test_guest_should_be_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+
+
+#def test_guest_should_see_login_link(browser):
+    #link = "http://selenium1py.pythonanywhere.com/"
+    #page = MainPage(browser, link)
+    #page.open()
+    #page.should_be_login_link()
+
+#def test_guest_should_be_login_page(browser):
+    #link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    #page = LoginPage(browser, link)
+    #page.open()
+    #page.should_be_login_page()
